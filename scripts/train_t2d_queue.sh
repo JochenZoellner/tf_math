f5d4569b61d2b0ae4e64ff2f96a9520bfdc07aab
jochen <jochen.zoellner@uni-rostock.de>
2020-02-19 16:40:06 +0100

    fix: lr 0.001 in start script

--- scripts/train_t2d_queue.sh
+++ scripts/train_t2d_queue.sh
@@ -18,7 +18,7 @@ PARAMS="
   --loss_mode point_diff
   --input_fn_params min_fov=1.0 max_fov=180.0
   --gpu_devices 0
-  --optimizer_params learning_rate 0.0001
+  --optimizer_params learning_rate=0.001
 "
 
 PYTHONPATH=/home/$USER/devel/projects/projectneiss2d/tf_neiss:$PYTHONPATH python -u ./tf_neiss/trainer/trainer_types/trainer_2dt/trainer_triangle2d.py ${PARAMS} "$@"
@@ -29,6 +29,6 @@ LAV_PARAMS="
   --batch_limiter 1
   "
 
-CUDA_VISIBLE_DEVICES="" PYTHONPATH=/home/$USER/devel/projects/projectneiss2d/tf_neiss:$PYTHONPATH python -u ./tf_neiss/trainer/lav_types/lav_triangle2d.py ${LAV_PARAMS} "$@" >> "models/dataset_${DATASET}/${ID}/lav-${ID}.log" 2>&1
+CUDA_VISIBLE_DEVICES="" PYTHONPATH=/home/$USER/devel/projects/projectneiss2d/tf_neiss:$PYTHONPATH python -u ./tf_neiss/trainer/lav_types/lav_triangle2d.py ${LAV_PARAMS} >> "models/dataset_${DATASET}/${ID}/lav-${ID}.log" 2>&1
 
 # example call: $> sh ./tf_bic/train_bic_queue.s
