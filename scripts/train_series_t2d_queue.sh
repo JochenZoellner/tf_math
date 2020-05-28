9d3fd14e525aac5e6465d5cd14c6734bc4661c45
jochen <jochen.zoellner@planet.de>
2020-05-28 10:27:53 +0200

    fix script

--- scripts/train_series_t2d_queue.sh
+++ scripts/train_series_t2d_queue.sh
@@ -10,27 +10,28 @@ set -xe
 
 DATASET="t2d_tf_314_1M_po_B"
 DATALEN=314
-
+mkdir -p models/dataset_${DATASET}/${ID_SERIES}
 for parameter in 0.0 5.0 10.0 #20.0 30.0 40.0 50.0 70.0 100.0 130.0 160.0
 do
     ID="${ID_SERIES}_${parameter}"
     PARAMS="
-      --train_lists infinity
+      --train_lists lists/${DATASET}_train.lst
       --val_list lists/${DATASET}_val.lst
-      --checkpoint_dir models/dataset_${DATASET}/${ID}
+      --checkpoint_dir models/dataset_${DATASET}/${ID_SERIES}/${ID}
       --epochs $EPOCH
+      --model_type ModelTriangleArea
       --graph GraphConv1MultiFF
-      --graph_params area_red=True pre_point_out=False
+      --graph_params area_pred=True pre_points_out=False
       --loss_mode relativeError
       --print_to file
       --data_len ${DATALEN}
+      --calc_ema True
       --optimizer FinalDecayOptimizer
       --input_fn_params min_fov=${parameter} max_fov=180.0
       --optimizer_params learning_rate=0.001 lr_decay_rate=0.9
-"#      --gpu_devices $GPU
-    #"
+"
 
-    TS_SOCKET=${QUEUE} tsp PYTHONPATH=/home/$USER/devel/projects/projectneiss2d/tf_neiss:$PYTHONPATH python -u ./tf_neiss/trainer/trainer_types/trainer_2dt/trainer_triangle2d.py ${PARAMS} "$@"
+    TS_SOCKET=${QUEUE} PYTHONPATH=/home/$USER/devel/projects/projectneiss2d/tf_neiss:$PYTHONPATH tsp  python -u ./tf_neiss/trainer/trainer_types/trainer_2dt/trainer_triangle2d_area.py ${PARAMS} "$@"
 #    LAV_PARAMS="
 #      --val_list lists/${DATASET}_val.lst
 #      --model_dir models/dataset_${DATASET}/${ID}
