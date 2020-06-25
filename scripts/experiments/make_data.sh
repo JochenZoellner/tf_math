69f7cd7e54a1de05bf22c4a31897aed76505ed6c
jochen <jochen.zoellner@planet.de>
2020-06-25 12:19:32 +0200

    make data min_fov script, fix --mode train

--- scripts/experiments/make_data.sh
+++ scripts/experiments/make_data.sh
@@ -5,10 +5,16 @@ DATASET="t2d_min_fov_exp"
 PARAMS="\
     --print_to both \
     --mode "val" \
-    --data_id t2d_312symetric_unbatchedB \
+    --data_id ${DATASET} \
     --centered False
     "
 # make val data
 CUDA_VISIBLE_DEVICES="" PYTHONPATH=/home/$USER/devel/projects/projectneiss2d/tf_neiss:$PYTHONPATH python -u ./tf_neiss/input_fn/input_fn_2d/data_gen_2dt/data_generator_t2d.py ${PARAMS} "$@"
 # make train data
-CUDA_VISIBLE_DEVICES="" PYTHONPATH=/home/$USER/devel/projects/projectneiss2d/tf_neiss:$PYTHONPATH python -u ./tf_neiss/input_fn/input_fn_2d/data_gen_2dt/data_generator_t2d.py "${PARAMS} --mode train $@"
+PARAMS="\
+    --print_to both \
+    --mode "train" \
+    --data_id ${DATASET} \
+    --centered False
+    "
+CUDA_VISIBLE_DEVICES="" PYTHONPATH=/home/$USER/devel/projects/projectneiss2d/tf_neiss:$PYTHONPATH python -u ./tf_neiss/input_fn/input_fn_2d/data_gen_2dt/data_generator_t2d.py ${PARAMS} "$@"
