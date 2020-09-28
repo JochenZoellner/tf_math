ad5947154b5e397ce17d9de111ddebceeaf0c09b
jochen <jochen.zoellner@planet-ai.de>
2020-09-28 14:43:38 +0200

    update plot_t2d.py, select mode, filename change

--- scripts/experiments/find_ambiguous_triangle/eval_t2d_find_ambiguous.sh
+++ scripts/experiments/find_ambiguous_triangle/eval_t2d_find_ambiguous.sh
@@ -22,7 +22,8 @@ mkdir -p models/dataset_${DATASET}
       --data_len ${DATALEN}
       --batch_limiter -1
       "
-    CUDA_VISIBLE_DEVICES="" PYTHONPATH=/home/$USER/devel/projects/projectneiss2d/tf_neiss:$PYTHONPATH python -u ./tf_neiss/trainer/lav_types/lav_triangle2d.py ${LAV_PARAMS} >> "models/dataset_${DATASET}/${ID}/lav-${ID}.log" 2>&1
+    # shellcheck disable=SC2068
+    CUDA_VISIBLE_DEVICES="" PYTHONPATH=/home/$USER/devel/projects/projectneiss2d/tf_neiss:$PYTHONPATH python -u ./tf_neiss/trainer/lav_types/lav_triangle2d.py "${LAV_PARAMS}" ${@} >> "models/dataset_${DATASET}/${ID}/lav-${ID}.log" 2>&1
 
 << ////
 # example call:
